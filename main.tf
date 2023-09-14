f780b4246b295a3441b4b07572b26d8c2e72ea2d
sabariarun <33651783+sabariarun@users.noreply.github.com>
2023-09-14 14:55:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,21 +63,39 @@ resource "aws_instance" "worker" {
   key_name             = var.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2connectprofile.name
   security_groups      = ["${local.name}-k8s-master-sec-gr"]
-  #!/bin/bash
-  sudo apt update -y
-  sudo apt install docker.io -y
-  sudo systemctl enable docker.service
-  sudo usermod -aG docker ubuntu
-  apt install -y apt-transport-https curl
-  sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
-  sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-  sudo apt update -y
-  sudo apt install -y kubelet kubeadm kubectl
-  sudo sysctl -w net.ipv4.ip_forward=1
-  sudo sed -i 's/net.ipv4.ip_forward=0/net.ipv4.ip_forward=1/Ig' /etc/sysctl.conf
-  kubeadm join ${aws_instance.Master-Node.private_ip}:6443 \
-  --token ${local.token} \
-  --discovery-token-unsafe-skip-ca-verification
+   user_data              = <<-EOF
+  #! /bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo hostnamectl set-hostname kube-worker
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet=1.26.3-00 kubeadm=1.26.3-00 kubectl=1.26.3-00 kubernetes-cni docker.io
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+sudo newgrp docker
+sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF>>
+sudo sysctl --system
+sudo mkdir /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+sudo wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+sudo pip install pyopenssl --upgrade
+sudo pip3 install ec2instanceconnectcli
+sudo apt install -y mssh
+sudo until [[ $(mssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -t ${master-id} -r ${region} ubuntu@${master-id} kubectl get no | awk 'NR == 2 {print $2}') == Ready ]]; do echo "master node is not ready"; sleep 3; done;
+sudo kubeadm join ${master-private}:6443 --token $(mssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -t ${master-id} -r ${region} ubuntu@${master-id} kubeadm token list | awk 'NR == 2 {print $1}') --discovery-token-ca-cert-hash sha256:$(mssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -t ${master-id} -r ${region} ubuntu@${master-id} openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //') --ignore-preflight-errors=All
   EOF
   tags = {
     Name = "${local.name}-kube-worker"
