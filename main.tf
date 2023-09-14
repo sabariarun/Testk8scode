e335e11299bd83c7993c11393cce43ce7e7387ae
sabariarun <33651783+sabariarun@users.noreply.github.com>
2023-09-14 14:42:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,18 +37,18 @@ net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
-sodo sysctl --system
-sodo mkdir /etc/containerd
-sodo containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
-sodo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-sodo systemctl restart containerd
-sodo systemctl enable containerd
-sodo kubeadm config images pull
-sodo kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=All
-sodo mkdir -p /home/ubuntu/.kube
-sodo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
-sodo chown ubuntu:ubuntu /home/ubuntu/.kube/config
-sodo su - ubuntu -c 'kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml'
+sudo sysctl --system
+sudo mkdir /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+sudo kubeadm config images pull
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=All
+sudo mkdir -p /home/ubuntu/.kube
+sudo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
+sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config
+sudo su - ubuntu -c 'kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml'
   EOF
 
   tags = {
