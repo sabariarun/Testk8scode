78217fe9f1316eac8f053f947402fc6d35b2a18f
sabariarun <33651783+sabariarun@users.noreply.github.com>
2023-09-21 23:24:50 +0530

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get upgrade -y
 hostnamectl set-hostname kube-worker
 apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet=1.26.3-00 kubeadm=1.26.3-00 kubectl=1.26.3-00 kubernetes-cni docker.io
